04a6b23ea7575c28125f49b26f855a84da191b22
secretmedellin <daobartender@gmail.com>
2023-06-06 07:10:44 -0700

    first commit navbar

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <div className="row">
+          {/* <div className="col-lg-8 col-md-6 col-sm-5 left-container"> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

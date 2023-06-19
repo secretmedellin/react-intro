5ed4900b461bd648dc03f278e3bed04a5c8c0472
Luyao Pan <luyao.ct@mstsolutions.com>
2023-06-18 21:20:15 -0700

    fix carousal

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import ImageCarousel from "./ImageCarousel";
+import pic_01 from "../Images/IMG_0790.JPG";
+import pic_02 from "../Images/IMG_9033.jpg";
 
 const ComingSoon = (props) => {
+  const imgs = [
+    { src: pic_01, text: "calles" },
+    { src: pic_02, text: "military hat" },
+  ];
   return (
     <div>
       <h1> ComingSoon</h1>
+      <ImageCarousel imgs={imgs} />;
     </div>
   );
 };

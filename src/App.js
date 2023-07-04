ba93ca23cc094acde8bc42882028b83171ca7c1e
secretmedellin <daobartender@gmail.com>
2023-07-04 12:39:54 -0400

    Fix image carousel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Navbar from "./components/Navbar";
 import { useState } from "react";
-// import logo from "./logo.svg";
 import "./App.css";
 import Collections from "./components/Collections";
 import ComingSoon from "./components/ComingSoon";
@@ -9,7 +8,6 @@ import OnSale from "./components/OnSale";
 import Hoodies from "./components/Hoodies";
 import Tshirts from "./components/Tshirts";
 import Hats from "./components/Hats";
-import YouTubeVD from "./components/YouTube";
 import ImageModal from "./components/ImageModal";
 
 function App() {
@@ -45,20 +43,14 @@ function App() {
                 setCurActiveMenu={setCurActiveMenu}
               />
             )}
-            {activeMenu === "ComingSoon" && <ComingSoon />}
+            {activeMenu === "ComingSoon" && (
+              <ComingSoon onClickItem={onClickItem} />
+            )}
             {activeMenu === "AboutUs" && <AboutUs />}
             {activeMenu === "OnSale" && <OnSale />}
             {activeMenu === "Hoodies" && <Hoodies />}
             {activeMenu === "Tshirts" && <Tshirts />}
             {activeMenu === "Hats" && <Hats />}
-            <ComingSoon onClickItem={onClickItem} />
-          </div>
-          <div
-            className="col-lg-4 col-md-6 col-sm-7 right-container"
-            id="second_container_color"
-          >
-            <h1>hello</h1>
-            <YouTubeVD />
           </div>
           <ImageModal
             imgSrc={selectedImgSrc}
--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ImageCarousel from "./ImageCarousel";
+import YouTubeVD from "./YouTube";
 import caru_03 from "../Images/caru_03.JPG";
 import caru_01 from "../Images/caru_01.JPG";
 import caru_04 from "../Images/caru_04.jpeg";
 import caru_02 from "../Images/caru_02.JPG";
 
 const ComingSoon = (props) => {
+  // const [selectedImgSrc, setSelectedImageSrc] = useState("");
+  // const [modalIsOpen, setIsOpen] = useState(false);
+
   const imgs = [
     { src: caru_01, text: "coming soon" },
     { src: caru_02, text: "coming soon" },
@@ -15,7 +19,14 @@ const ComingSoon = (props) => {
   const onClickItem = (index, item) => {
     props.onClickItem(imgs[index].src);
   };
+  // <ComingSoon onClickItem={onClickItem} />;
 
-  return <ImageCarousel imgs={imgs} onClickItem={onClickItem} />;
+  return (
+    <>
+      <h1>hello</h1>
+      <YouTubeVD />
+      <ImageCarousel imgs={imgs} onClickItem={onClickItem} />
+    </>
+  );
 };
 export default ComingSoon;
